7c450ec3c5e359f66f9d16632f3e79bff29010b2
Aviel Segev <asegev@redhat.com>
2026-01-08 13:15:52 +0200

    feat: add clone command group

---
+++ internal/cli/clone/clone.go
@@ -0,0 +1,23 @@
+package clone
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// NewCloneCmd creates the clone command
+func NewCloneCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "clone",
+		Short: "Clone virtual machines",
+		Long: `Clone virtual machines with basic cloning capabilities.
+
+Available subcommands:
+  create  - Create a full clone of a VM
+  list    - List cloned VMs`,
+	}
+
+	cmd.AddCommand(newCreateCmd())
+	cmd.AddCommand(newListCmd())
+
+	return cmd
+}

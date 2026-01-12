ea800e94db069cc4d8736e5b810a5ed81e79192b
Aviel Segev <asegev@redhat.com>
2026-01-12 14:38:48 +0200

    testing the credentials test methos

--- internal/cli/credentials/test.go
+++ internal/cli/credentials/test.go
@@ -1,10 +1,25 @@
 package credentials
 
 import (
-	"errors"
+	"fmt"
+	"github.com/asegev/vsphere-cli/pkg/config"
 
+	vmware "github.com/kubev2v/assisted-migration-agent/pkg/vmware"
 	"github.com/spf13/cobra"
 )
+
+const (
+	defaultDatacenterMoid = "datacenter-3"
+	defaultClusterMoid    = "domain-c34"
+	defaultVmName         = "asegev-ubuntu-for-workflow-runs"
+)
+
+var requiredPrivileges = []string{
+	"VirtualMachine.Provisioning.Clone",
+	"VirtualMachine.Inventory.CreateFromExisting",
+	"VirtualMachine.State.CreateSnapshot",
+	"VirtualMachine.State.RemoveSnapshot",
+}
 
 func newTestCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -20,7 +35,32 @@ This command will:
 
 Exit code 0 on success, 1 on failure.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return errors.New("not implemented")
+			ctx := cmd.Context()
+
+			cfg, err := config.LoadFromEnv()
+
+			c, err := vmware.NewVsphereClient(ctx, cfg.Host, cfg.Username, cfg.Password, cfg.Insecure)
+			if err != nil {
+				return err
+			}
+
+			finder, err := vmware.NewFinder(ctx, c.Client, defaultDatacenterMoid)
+			if err != nil {
+				return err
+			}
+
+			vm, err := finder.FindVMByName(ctx, defaultVmName)
+			if err != nil {
+				return err
+			}
+
+			if err := vmware.ValidateUserPrivileges(ctx, c.Client, vm.Reference(), requiredPrivileges, cfg.Username); err != nil {
+				return err
+			}
+
+			fmt.Println("Successfully validated credentials.")
+
+			return nil
 		},
 	}
 
